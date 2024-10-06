984160af87f6923e98231f5769b47832e6f25280
melnikov412 <mymontag@gmail.com>
2024-10-06 21:15:51 +0300

    to-do-list-item

--- todo-app-16/src/app/app.module.ts
+++ todo-app-16/src/app/app.module.ts
@@ -13,11 +13,13 @@ import {MatChipsModule} from '@angular/material/chips';
 import {MatFormFieldModule} from '@angular/material/form-field';
 import {MatInputModule} from '@angular/material/input';
 import {FormsModule} from '@angular/forms';
+import { ToDoListItemComponent } from './components/to-do-list-item/to-do-list-item.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    ToDoListComponent
+    ToDoListComponent,
+    ToDoListItemComponent
   ],
   imports: [
     BrowserModule,
